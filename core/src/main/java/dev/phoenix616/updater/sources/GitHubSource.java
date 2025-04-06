26c6faebca7f3cbc0a5f7ecb6c1a7dd1adccab4a
Phoenix616 <max@themoep.de>
2025-04-06 15:09:36 +0200

    Abstract away duplicate code in GitHub source

--- core/src/main/java/dev/phoenix616/updater/sources/GitHubSource.java
+++ core/src/main/java/dev/phoenix616/updater/sources/GitHubSource.java
@@ -59,63 +59,9 @@ public class GitHubSource extends UpdateSource {
     @Override
     public String getLatestVersion(PluginConfig config) {
         try {
-            List<String> properties = new ArrayList<>(Arrays.asList("Accept", API_HEADER));
-            if (config.getParameters().containsKey("token")) {
-                Collections.addAll(properties, "Authorization", "token " + config.getParameters().get("token"));
-            } else if (config.getParameters().containsKey("username") && config.getParameters().containsKey("password")) {
-                String userPass = config.getParameters().get("username") + ":" + config.getParameters().get("password");
-                Collections.addAll(properties, "Authorization", "Basic " + Base64.getEncoder().encodeToString(userPass.getBytes()));
-            }
-            String channel = config.getParameters().get("channel");
-            if (channel != null && !"release".equals(channel) && !"prerelease".equals(channel)) {
-                updater.log(Level.SEVERE, "Invalid channel '" + channel + " for " + config.getName() + " from source " + getName() + "! Must be 'release' or 'prerelease'.");
-                return null;
-            }
-            String draft = config.getParameters().get("draft");
-            String author = config.getParameters().get("author");
-            String s = updater.query(new URL(new Replacer().replace(config.getParameters("repository")).replaceIn(RELEASES_URL)), properties.toArray(new String[0]));
-            if (s != null) {
-                try {
-                    JsonElement json = new JsonParser().parse(s);
-                    if (json.isJsonArray() && ((JsonArray) json).size() > 0) {
-                        for (JsonElement release : ((JsonArray) json)) {
-                            if (release.isJsonObject()
-                                    && ((JsonObject) release).has("tag_name")
-                                    && ((JsonObject) release).has("assets")
-                                    && ((JsonObject) release).get("assets").isJsonArray()) {
-                                boolean isPrerelease = ((JsonObject) release).has("prerelease") && ((JsonObject) release).get("prerelease").getAsBoolean();
-                                boolean isDraft = ((JsonObject) release).has("draft") && ((JsonObject) release).get("draft").getAsBoolean();
-                                if (isDraft && !"true".equalsIgnoreCase(draft)) {
-                                    continue;
-                                }
-                                if ("release".equalsIgnoreCase(channel) && isPrerelease) {
-                                    continue;
-                                } else if ("prerelease".equalsIgnoreCase(channel) && !isPrerelease) {
-                                    continue;
-                                }
-
-                                if (author != null && !author.isEmpty() && ((JsonObject) release).has("author")) {
-                                    String authorName = ((JsonObject) release).get("author").getAsJsonObject().get("login").getAsString();
-                                    if (!author.equalsIgnoreCase(authorName)) {
-                                        continue;
-                                    }
-                                }
-
-
-                                for (JsonElement asset : ((JsonObject) release).getAsJsonArray("assets")) {
-                                    if (matches(config, asset)) {
-                                        return ((JsonObject) release).get("tag_name").getAsString();
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    updater.log(Level.WARNING, "Json did not contain release entry for " + config.getName() + " from source " + getName() + ": " + json);
-                } catch (JsonParseException e) {
-                    updater.log(Level.SEVERE, "Invalid Json returned when getting latest version for " + config.getName() + " from source " + getName() + ": " + s + ". Error: " + e.getMessage());
-                }
-            } else {
-                updater.log(Level.WARNING, "Query didn't return anything for " + config.getName() + " from source " + getName() + "!");
+            Release release = getRelease(config);
+            if (release != null) {
+                return release.tagName();
             }
         } catch (MalformedURLException e) {
             updater.log(Level.SEVERE, "Invalid URL for getting latest version for " + config.getName() + " from source " + getName() + "! " + e.getMessage());
@@ -125,6 +71,14 @@ public class GitHubSource extends UpdateSource {
 
     @Override
     public URL getUpdateUrl(PluginConfig config) throws MalformedURLException, FileNotFoundException {
+        Release release = getRelease(config);
+        if (release != null) {
+            return new URL(release.downloadUrl());
+        }
+        throw new FileNotFoundException("Not found");
+    }
+
+    private Release getRelease(PluginConfig config) throws MalformedURLException {
         List<String> properties = new ArrayList<>(Arrays.asList("Accept", API_HEADER));
         if (config.getParameters().containsKey("token")) {
             Collections.addAll(properties, "Authorization", "token " + config.getParameters().get("token"));
@@ -137,13 +91,13 @@ public class GitHubSource extends UpdateSource {
             updater.log(Level.SEVERE, "Invalid channel '" + channel + " for " + config.getName() + " from source " + getName() + "! Must be 'release' or 'prerelease'.");
             return null;
         }
-        String draft = config.getParameters().get("draft");
-        String author = config.getParameters().get("author");
         String s = updater.query(new URL(new Replacer().replace(config.getParameters("repository")).replaceIn(RELEASES_URL)), properties.toArray(new String[0]));
         if (s != null) {
             try {
                 JsonElement json = new JsonParser().parse(s);
                 if (json.isJsonArray() && ((JsonArray) json).size() > 0) {
+                    String draft = config.getParameters().get("draft");
+                    String author = config.getParameters().get("author");
                     for (JsonElement release : ((JsonArray) json)) {
                         if (release.isJsonObject()
                                 && ((JsonObject) release).has("tag_name")
@@ -169,17 +123,24 @@ public class GitHubSource extends UpdateSource {
 
                             for (JsonElement asset : ((JsonObject) release).getAsJsonArray("assets")) {
                                 if (matches(config, asset)) {
-                                    return new URL(((JsonObject) asset).get("browser_download_url").getAsString());
+                                    return new Release(
+                                            ((JsonObject) release).get("tag_name").getAsString(),
+                                            ((JsonObject) asset).get("name").getAsString(),
+                                            ((JsonObject) asset).get("browser_download_url").getAsString()
+                                    );
                                 }
                             }
                         }
                     }
                 }
+                updater.log(Level.WARNING, "Json did not contain release entry for " + config.getName() + " from source " + getName() + ": " + json);
             } catch (JsonParseException e) {
                 updater.log(Level.SEVERE, "Invalid Json returned when getting latest version for " + config.getName() + " from source " + getName() + ": " + s + ". Error: " + e.getMessage());
             }
+        } else {
+            updater.log(Level.WARNING, "Query didn't return anything for " + config.getName() + " from source " + getName() + "!");
         }
-        throw new FileNotFoundException("Not found");
+        return null;
     }
 
     private boolean matches(PluginConfig config, JsonElement asset) {
@@ -208,65 +169,44 @@ public class GitHubSource extends UpdateSource {
     @Override
     public File downloadUpdate(PluginConfig config) {
         try {
-            List<String> properties = new ArrayList<>(Arrays.asList("Accept", API_HEADER));
-            if (config.getParameters().containsKey("token")) {
-                Collections.addAll(properties, "Authorization", "token " + config.getParameters().get("token"));
-            } else if (config.getParameters().containsKey("username") && config.getParameters().containsKey("password")) {
-                String userPass = config.getParameters().get("username") + ":" + config.getParameters().get("password");
-                Collections.addAll(properties, "Authorization", "Basic " + Base64.getEncoder().encodeToString(userPass.getBytes()));
-            }
-            String s = updater.query(new URL(new Replacer().replace(config.getParameters("repository")).replaceIn(RELEASES_URL)), properties.toArray(new String[0]));
-            if (s != null) {
+            Release release = getRelease(config);
+            if (release != null) {
+                String name = release.assetName();
+                String version = release.tagName();
+                updater.log(Level.FINE, "Downloading update file for " + config.getName() + " " + version + ": " + name);
+                File target = new File(updater.getTempFolder(), config.getName() + "-" + version + "-" + name);
+
                 try {
-                    JsonElement json = new JsonParser().parse(s);
-                    if (json.isJsonArray() && ((JsonArray) json).size() > 0) {
-                        for (JsonElement release : ((JsonArray) json)) {
-                            if (release.isJsonObject()
-                                    && ((JsonObject) release).has("tag_name")
-                                    && ((JsonObject) release).has("assets")
-                                    && ((JsonObject) release).get("assets").isJsonArray()) {
-                                for (JsonElement asset : ((JsonArray) ((JsonObject) release).get("assets"))) {
-                                    if (matches(config, asset)) {
-                                        String name = ((JsonObject) asset).get("name").getAsString();
-                                        String version = ((JsonObject) release).get("tag_name").getAsString();
-                                        updater.log(Level.FINE, "Downloading update file for " + config.getName() + " " + version + ": " + name);
-                                        File target = new File(updater.getTempFolder(), config.getName() + "-" + version + "-" + name);
+                    URL source = new URL(release.downloadUrl());
 
-                                        try {
-                                            URL source = new URL(((JsonObject) asset).get("browser_download_url").getAsString());
-
-                                            HttpURLConnection con = (HttpURLConnection) source.openConnection();
-                                            con.setRequestProperty("User-Agent", updater.getUserAgent());
-                                            con.addRequestProperty("Accept", API_HEADER);
-                                            con.addRequestProperty("Accept", "application/octet-stream");
-                                            if (config.getParameters().containsKey("token")) {
-                                                con.addRequestProperty("Authorization", "token " + config.getParameters().get("token"));
-                                            } else if (config.getParameters().containsKey("username") && config.getParameters().containsKey("password")) {
-                                                String userPass = config.getParameters().get("username") + ":" + config.getParameters().get("password");
-                                                con.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString(userPass.getBytes()));
-                                            }
-                                            con.setUseCaches(false);
-                                            con.connect();
-                                            try (InputStream in = con.getInputStream()) {
-                                                if (Files.copy(in, target.toPath(), StandardCopyOption.REPLACE_EXISTING) > 0) {
-                                                    return target;
-                                                }
-                                            }
-                                        } catch (IOException e) {
-                                            updater.log(Level.SEVERE, "Error while trying to download update " + version + " for " + config.getName() + " from source " + getName() + "! " + e.getMessage());
-                                        }
-                                    }
-                                }
-                            }
+                    HttpURLConnection con = (HttpURLConnection) source.openConnection();
+                    con.setRequestProperty("User-Agent", updater.getUserAgent());
+                    con.addRequestProperty("Accept", API_HEADER);
+                    con.addRequestProperty("Accept", "application/octet-stream");
+                    if (config.getParameters().containsKey("token")) {
+                        con.addRequestProperty("Authorization", "token " + config.getParameters().get("token"));
+                    } else if (config.getParameters().containsKey("username") && config.getParameters().containsKey("password")) {
+                        String userPass = config.getParameters().get("username") + ":" + config.getParameters().get("password");
+                        con.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString(userPass.getBytes()));
+                    }
+                    con.setUseCaches(false);
+                    con.connect();
+                    try (InputStream in = con.getInputStream()) {
+                        if (Files.copy(in, target.toPath(), StandardCopyOption.REPLACE_EXISTING) > 0) {
+                            return target;
                         }
                     }
-                } catch (JsonParseException e) {
-                    updater.log(Level.SEVERE, "Invalid Json returned when getting latest version for " + config.getName() + " from source " + getName() + ": " + s + ". Error: " + e.getMessage());
+                } catch (IOException e) {
+                    updater.log(Level.SEVERE, "Error while trying to download update " + version + " for " + config.getName() + " from source " + getName() + "! " + e.getMessage());
                 }
             }
         } catch (MalformedURLException e) {
             updater.log(Level.SEVERE, "Invalid URL for getting latest version for " + config.getName() + " from source " + getName() + "! " + e.getMessage());
         }
         return null;
+    }
+
+    record Release(String tagName, String assetName, String downloadUrl) {
+
     }
 }
